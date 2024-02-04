1c416f005ee00c4d50499dbedb9fe2c24a233e2b
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-02-03 20:38:24 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Nikita Zhdanov on 2024-02-03.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
