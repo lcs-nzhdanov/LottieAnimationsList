bdf714065727e637b2d646c6399cf850959fe63a
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-02-03 20:45:09 -0500

    Added Mr. Gordon's helper view.

--- LottieAnimationsList/Views/AnimationsListView.swift
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -5,14 +5,48 @@
 //  Created by Nikita Zhdanov on 2024-02-03.
 //
 
+import UIKit
 import SwiftUI
+import Lottie
 
-struct AnimationsListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LottieView: UIViewRepresentable {
+
+    @State var animationNamed: String
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        
+        let view = UIView(frame: .zero)
+        
+        // Lottie View
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(animationNamed)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        // Constraints
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+        
     }
-}
-
-#Preview {
-    AnimationsListView()
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    typealias UIViewType = UIView
+    
+    struct LottieView_Previews: PreviewProvider {
+        static var previews: some View {
+            LottieView(animationNamed: "782-check-mark-success")
+        }
+    }
 }
