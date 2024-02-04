e80fd65c5ecb9592e68b27baa38ec5a75270bda1
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-02-03 20:30:04 -0500

    Initial Commit

---
+++ LottieAnimationsList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LottieAnimationsList
+//
+//  Created by Nikita Zhdanov on 2024-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Nikita Zhdanov on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
